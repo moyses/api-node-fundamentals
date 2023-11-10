7807a8090f632ffce782880993467a76a033a967
Moyses Viana <moysesspj@gmail.com>
2023-11-09 22:38:28 -0300

    created routes and types config and middlewares for cookies

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
+import cookie from '@fastify/cookie'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
+app.register(cookie)
 
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
