2da9ac50a1e8d1e72c346cdb70c5f4bbfdc837c0
Moyses Viana <moysesspj@gmail.com>
2023-11-12 16:58:36 -0300

    add db local empty and isolate for test e2e

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,7 +12,14 @@ describe('Tansactions routes', () => {
     await app.close()
   })
 
-  test('use can create a new transaction', async () => {
+  beforeEach(() => {
+    // this rollback ensures that each database
+    // is clean and isolated to run your test
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -20,5 +28,29 @@ describe('Tansactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transactions',
+        amount: 3000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsRespnse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsRespnse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new transactions',
+        amount: 3000,
+      }),
+    ])
   })
 })
